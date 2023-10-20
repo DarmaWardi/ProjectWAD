497b63018a7befd1dd8cb8610dd609c3b2039b80
DarmaWardi <121552909+DarmaWardi@users.noreply.github.com>
2023-10-20 17:36:30 +0700

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Latihan1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+
+  <!-- Example single danger button -->
+<div class="btn-group">
+  <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+    Action
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+    <li><hr class="dropdown-divider"></li>
+    <li><a class="dropdown-item" href="#">Separated link</a></li>
+  </ul>
+</div>
+ <body>
+
+<script>
+function clickCounter() {
+  if (typeof(Storage) !== "undefined") {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount)+1;
+    } else {
+      localStorage.clickcount = 1;
+    }
+    document.getElementById("result").innerHTML = "Kamu Sudah Menekan " + localStorage.clickcount + " kali";
+  } else {
+    document.getElementById("result").innerHTML = "yahh...";
+  }
+}
+</script>
+<body>
+
+<p><button onclick="clickCounter()" type="button">Tekan Aku!</button></p>
+<div id="result"></div>
+<p>Tekan untuk menambahkan angka.</p>
+<p>Tutup tab untuk mereset</p>
+
+</body>
+</html>
+
+
+
+
+  </body>
+</html>
